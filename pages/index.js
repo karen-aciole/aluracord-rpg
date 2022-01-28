0a8e56732a7dac7ae7aafa18b9f8f3a7abb0fefb
Karen Aciole <karen.alves@ccc.ufcg.edu.br>
2022-01-28 13:31:45 -0300

    add default image and changes on box layout

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
 
 function Title(props) {
     const Tag = props.tag || 'h1';
@@ -47,26 +20,13 @@ function Title(props) {
     );
 }
 
-// // Component React
-// function HomePage() {
-//     //JSX 
-//     return ( 
-//         <div>
-//             <GlobalStyle />
-//                 <Title tag="h2">Roll your dices!</Title>
-//                 <h2>Discord - Alura </h2>
-//         </div>
-//     )
-//   }
-
-//     export default HomePage
-
 export default function PaginaInicial() {
-    const username = 'karen-aciole';
+    const [username, setUsername] = React.useState();
+    const roteamento = useRouter();
+    const defaultImage = 'https://cdn.icon-icons.com/icons2/1356/PNG/512/if-orc-2913114_88595.png';
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -87,12 +47,18 @@ export default function PaginaInicial() {
                         width: '100%', maxWidth: '700px',
                         borderRadius: '5px', padding: '32px', margin: '16px',
                         boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-                        backgroundColor: appConfig.theme.colors.neutrals[700],
+                        backgroundColor: appConfig.theme.colors.opacity['bg-box'],
+                        
                     }}
                 >
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (infosDoEvento) {
+                            infosDoEvento.preventDefault();
+                            console.log('Alguém submeteu o form');
+                            roteamento.push('/chat');
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -104,13 +70,19 @@ export default function PaginaInicial() {
                         </Text>
 
                         <TextField
+                            value={username}
+                            onChange={function (event) {
+                                console.log('usuario digitou', event.target.value);
+                                const valor = event.target.value;
+                                setUsername(valor);
+                            }}
                             fullWidth
                             textFieldColors={{
                                 neutral: {
                                     textColor: appConfig.theme.colors.neutrals[200],
-                                    mainColor: appConfig.theme.colors.neutrals[900],
-                                    mainColorHighlight: appConfig.theme.colors.primary[500],
-                                    backgroundColor: appConfig.theme.colors.neutrals[800],
+                                    mainColor: appConfig.theme.colors.opacity['field-form'],
+                                    mainColorHighlight: appConfig.theme.colors.primary[800],
+                                    backgroundColor: appConfig.theme.colors.opacity['field-form']
                                 },
                             }}
                         />
@@ -137,9 +109,9 @@ export default function PaginaInicial() {
                             alignItems: 'center',
                             maxWidth: '200px',
                             padding: '16px',
-                            backgroundColor: appConfig.theme.colors.neutrals[800],
+                            backgroundColor: appConfig.theme.colors.opacity['photo'],
                             border: '1px solid',
-                            borderColor: appConfig.theme.colors.neutrals[999],
+                            borderColor: appConfig.theme.colors.opacity['photo'],
                             borderRadius: '10px',
                             flex: 1,
                             minHeight: '240px',
@@ -150,13 +122,16 @@ export default function PaginaInicial() {
                                 borderRadius: '50%',
                                 marginBottom: '16px',
                             }}
-                            src={`https://github.com/${username}.png`}
+                            onError={function(event) {
+                                event.target.src = defaultImage;
+                            }}
+                            src={username.length > 2 ? `https://github.com/${username}.png` : defaultImage}
                         />
                         <Text
                             variant="body4"
                             styleSheet={{
                                 color: appConfig.theme.colors.neutrals[200],
-                                backgroundColor: appConfig.theme.colors.neutrals[900],
+                                backgroundColor: appConfig.theme.colors.neutrals[800],
                                 padding: '3px 10px',
                                 borderRadius: '1000px'
                             }}
